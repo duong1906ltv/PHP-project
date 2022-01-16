b475cff96813f5d610775e52c6043933407accfa
Dinh Duong <duong1906ltv@gmail.com>
2022-01-16 23:25:59 +0700

    create the admin category page

--- includes/navigation.php
+++ includes/navigation.php
@@ -22,7 +22,7 @@
                     }
                     ?>
                     <li>
-                        <a href="#">About</a>
+                        <a href="admin/">Admin</a>
                     </li>
                     <li>
                         <a href="#">Services</a>
