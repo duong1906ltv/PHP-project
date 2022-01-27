2ae4d5d208deab63c7dd618fc3dc7a68c8421005
Dinh Duong <duong1906ltv@gmail.com>
2022-01-27 23:41:42 +0700

    change update system

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,3 +1,70 @@
+<?php
+
+if (isset($_GET['p_id'])) {
+    $the_post_id = $_GET['p_id'];
+}
+
+$query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+$select_posts_by_id = mysqli_query($connection, $query);
+
+while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
+    $post_id = $row['post_id'];
+    $post_author = $row['post_author'];
+    $post_title = $row['post_title'];
+    $post_category_id = $row['post_category_id'];
+    $post_status = $row['post_status'];
+    $post_image = $row['post_image'];
+    $post_content = $row['post_content'];
+    $post_tags = $row['post_tags'];
+    $post_comment_count = $row['post_comment_count'];
+    $post_date = $row['post_date'];
+}
+
+if (isset($_POST['update_post'])) {
+    $post_author = $_POST['post_author'];
+    $post_title = $_POST['post_title'];
+    $post_category_id = $_POST['post_category'];
+    $post_status = $_POST['post_status'];
+    $post_tags = $_POST['post_tags'];
+
+    $post_content = $_POST['post_content'];
+    $post_content = mysqli_real_escape_string($connection, $post_content);
+    // Or use htmlentities() vs html_entity_decode()
+
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    if (empty($post_image)) {
+        $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+        $select_image = mysqli_query($connection, $query);
+
+        while ($row = mysqli_fetch_assoc($select_image)) {
+            $post_image = $row['post_image'];
+        }
+    }
+
+    $query = "UPDATE posts SET ";
+    $query .= "post_author = '{$post_author}', ";
+    $query .= "post_title = '{$post_title}', ";
+    $query .= "post_category_id = {$post_category_id}, ";
+    $query .= "post_status = '{$post_status}', ";
+    $query .= "post_date = now(), ";
+    $query .= "post_tags = '{$post_tags}', ";
+    $query .= "post_content = '{$post_content}', ";
+    $query .= "post_image = '{$post_image}' ";
+    $query .= "WHERE post_id = {$the_post_id}";
+
+    $update_post = mysqli_query($connection, $query);
+
+    confirmQuery($update_post);
+
+    echo "<p class='bg-success'>Post Created. <a href='../post.php?p_id=$post_id'>View Post</a> or <a href='posts.php'>Edit More Posts</a></p>";
+}
+
+?>
+
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="title">Post Title</label>
