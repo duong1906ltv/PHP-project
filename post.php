7b664778a93214896bfe436328743dbd3e02edbc
Dinh Duong <duong1906ltv@gmail.com>
2022-01-30 01:19:11 +0700

    insert comments from front end form

--- post.php
+++ post.php
@@ -48,7 +48,59 @@
                     <?php  
                     }
                 ?>
+                <!-- Blog Comments -->
+                <?php
 
+                if (isset($_POST['create_comment'])) {
+                    $the_post_id = $_GET['p_id'];
+
+                    $comment_author = $_POST['comment_author'];
+                    $comment_email = $_POST['comment_email'];
+                    $comment_content = $_POST['comment_content'];
+
+                    if (!empty($comment_author) && !empty($comment_email) && !empty($comment_content)) {
+                        $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ";
+                        $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now() )";
+
+                        $create_comment_query = mysqli_query($connection, $query);
+
+                        if (!$create_comment_query) {
+                            die("QUERY FAILED" . mysqli_error($connection));
+                        }
+
+                        $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
+                        $query .= "WHERE post_id = $the_post_id ";
+                        $update_comment_count = mysqli_query($connection, $query);
+
+                        if (!$update_comment_count) {
+                            die("QUERY FAILED" . mysqli_error($connection));
+                        }
+                    } else {
+                        echo "<script>alert('Fields cannot be empty')</script>";
+                    }
+                }
+
+                ?>
+
+                <!-- Comments Form -->
+                <div class="well">
+                    <h4>Leave a Comment:</h4>
+                    <form action="" method="post" role="form">
+                        <div class="form-group">
+                            <label for="author">Author</label>
+                            <input type="text" class="form-control" name="comment_author">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" name="comment_email">
+                        </div>
+                        <div class="form-group">
+                            <label for="comment">Your Comment</label>
+                            <textarea class="form-control" rows="3" name="comment_content"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary" name="create_comment">Submit</button>
+                    </form>
+                </div>
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
