f8fa90bfd364f2ab8c8f95ac4417fd051f7391f0
Dinh Duong <duong1906ltv@gmail.com>
2022-01-31 23:54:06 +0700

    create user page and link

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -41,10 +41,10 @@
                         <a href="./posts.php" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="demo" class="collapse">
                             <li>
-                                <a href="#">Dropdown Item</a>
+                                <a href="posts.php">View All Posts</a>
                             </li>
                             <li>
-                                <a href="#">Dropdown Item</a>
+                                <a href="userss.php?source=add_user">Add Post</a>
                             </li>
                         </ul>
                     </li>
@@ -52,13 +52,13 @@
                         <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
                     </li>
                     <li>
-                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
+                        <a href="./users.php" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="demo" class="collapse">
                             <li>
-                                <a href="#">Dropdown Item</a>
+                                <a href="users.php">View All Users</a>
                             </li>
                             <li>
-                                <a href="#">Dropdown Item</a>
+                                <a href="posts.php?source=add_user">Add User</a>
                             </li>
                         </ul>
                     </li>
