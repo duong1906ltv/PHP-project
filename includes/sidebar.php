271d21cee9c1a637cf1f60b0c66056e38de9eee3
Dinh Duong <duong1906ltv@gmail.com>
2022-02-02 21:57:48 +0700

    making the login page

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -19,12 +19,12 @@
 <!-- Login -->
 <div class="well">
     <h4>Blog Search</h4>
-    <form action="search.php" method="post">
+    <form action="includes/login.php" method="post">
     <div class="form-group">
         <input name="username" type="text" class="form-control" placeholder="Enter Username">
     </div>
     <div class="input-group">
-        <input name="password" type="text" class="form-control" placeholder="Enter Password">
+        <input name="password" type="password" class="form-control" placeholder="Enter Password">
         <span class="input-group-btn">
             <button class="btn btn-primary" name="login" type="submit">Submit</button>
 
