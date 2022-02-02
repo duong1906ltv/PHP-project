6afae87e1968cc364248b3e76eb8217daffff909
Dinh Duong <duong1906ltv@gmail.com>
2022-02-02 22:13:55 +0700

    validate user query in front end

--- includes/login.php
+++ includes/login.php
@@ -15,7 +15,24 @@ if(isset($_POST['login'])){
     }
     while($row = mysqli_fetch_array($select_user_query)){
         $db_id = $row["user_id"];
-        echo $db_id;
+        $db_username = $row["username"];
+        $db_user_password = $row["user_password"];
+        $db_user_firstname = $row["user_firstname"];
+        $db_user_lastname = $row["user_lastname"];
+        $db_user_role = $row["user_role"];
+    }
+    if ($username !== $db_username && $password !== $db_user_password){
+        
+        echo "1";
+    }
+    else if ($username == $db_username && $password == $db_user_password){
+        header("Location: ../admin/");
+        echo "2"; 
+    }
+    else{
+        
+        echo "3";
+
     }
 }
 
