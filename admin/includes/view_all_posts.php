a947ea2f3c58f0e5ddc0985837e81f33f01df8f3
Dinh Duong <duong1906ltv@gmail.com>
2022-02-06 00:00:26 +0700

    adding funcitionally to the post

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -83,6 +83,7 @@ if (isset($_POST['checkBoxArray'])){
             <th>Tags</th>
             <th>Comments</th>
             <th>Date</th>
+            <th>Views Count</th>
             <th>View Post</th>
             <th>Edit</th>
             <th>Delete</th>
@@ -103,6 +104,7 @@ if (isset($_POST['checkBoxArray'])){
             $post_comment_count = $row['post_comment_count'];
             $post_title = $row['post_title'];
             $post_tags = $row['post_tags'];
+            $post_views_count = $row['post_views_count'];
             echo "<tr>";
             ?>
                 <td><input type="checkbox" class="checkBoxes" name="checkBoxArray[]" value=<?php echo $post_id; ?>></td>
@@ -116,6 +118,7 @@ if (isset($_POST['checkBoxArray'])){
             echo "<td>{$post_tags}</td>";
             echo "<td>{$post_comment_count}</td>";
             echo "<td>{$post_date}</td>";
+            echo "<td>{$post_views_count}</td>";
             echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
             echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
             echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete?');\" href='posts.php?delete={$post_id}'>Delete</a></td>";
--- post.php
+++ post.php
@@ -19,7 +19,14 @@
                 <?php
                 if (isset($_GET['p_id'])) {
                     $the_post_id = $_GET['p_id'];
-                }
+                    
+                    $view_query = "UPDATE posts SET post_views_count = post_views_count + 1 WHERE post_id = $the_post_id ";
+                    $send_query = mysqli_query($connection, $view_query);
+                    
+                    if (!$send_query){
+                        die("query failed");
+                    }
+                
 
                 $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
                 $select_all_posts_query = mysqli_query($connection, $query);
@@ -47,6 +54,9 @@
                     <hr>
                     <?php  
                     }
+                }else{
+                    header("Location: index.php");
+                }
                 ?>
                 <!-- Blog Comments -->
                 <?php
