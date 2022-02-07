cbd43f937a3097b44a9260bb06204bb85b07d6c9
Dinh Duong <duong1906ltv@gmail.com>
2022-02-07 08:51:39 +0700

    pagination

--- index_old.php
+++ index_old.php
@@ -17,24 +17,7 @@
                     <small>Secondary Text</small>
                 </h1>
                 <?php
-                    $per_page = 2;
-                    if (isset($_GET['page'])) {
-                        $page = $_GET['page'];
-                    } else {
-                        $page = "";
-                    }
-        
-                    if ($page == "" || $page == 1) {
-                        $page_1 = 0;
-                    } else {
-                        $page_1 = ($page * $per_page) - $per_page;
-                    }
-                    $post_query_count = "SELECT * FROM posts";
-                    $find_count = mysqli_query($connection, $post_query_count);
-                    $count = mysqli_num_rows($find_count);
-                    $count = ceil($count/$per_page);
-
-                    $query = "SELECT * FROM posts LIMIT $page_1,$per_page";
+                    $query = "SELECT * FROM posts";
                     $select_all_posts_query = mysqli_query($connection,$query);
                     while($row = mysqli_fetch_assoc($select_all_posts_query)){
                         $post_id =$row['post_id'];
@@ -70,6 +53,18 @@
                 ?>
  
 
+
+
+                <!-- Pager -->
+                <ul class="pager">
+                    <li class="previous">
+                        <a href="#">&larr; Older</a>
+                    </li>
+                    <li class="next">
+                        <a href="#">Newer &rarr;</a>
+                    </li>
+                </ul>
+
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
@@ -77,17 +72,7 @@
 
         </div>
         <!-- /.row -->
-        <ul class="pager">
-            <?php
-            for ($i = 1; $i <= $count; $i++) {
-                if ($i == $page) {
-                    echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
-                } else {
-                    echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
-                }
-            }
-            ?>
-        </ul>
+
         <hr>
 <?php
 include "includes/footer.php"
